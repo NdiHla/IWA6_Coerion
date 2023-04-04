a01f1a958261342f178c2d424365e4b24d0fdcf5
Ndimande <125868614+NdiHla@users.noreply.github.com>
2023-04-04 23:18:02 +0200

    challenge 2

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = typeof number == primaryPhone
+const secondaryValid = typeof number == secondaryPhone
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid)
+
+
+const rent = 400;
+const tax = '12%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+
+if ('hourOfDay', minuteOfDay !== null, hourOfDay == '00', minuteOfDay == '00') {
+	const taxAsDecimal = "0.12" / '100'
+  const startingAfterTax = salary * '800' - 0.12
+	const balace = 704 - 10.2 - 51.75 - 400 == 242.05
+}
+	
+console.log(balance.toFixed(242.05))
