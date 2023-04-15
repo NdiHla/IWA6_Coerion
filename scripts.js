29e11271e4b5d4f38d00cd84d6a0d17741f855db
Ndimande <mkatroso.deep@gmail.com>
2023-04-15 21:33:08 +0200

    Coerion

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
-const primaryPhone = 'O748105141'
+const primaryPhone = '0748105141'
 const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = typeof number == 'O748105141'
-const secondaryValid = typeof number == '0219131568'
+const primaryValid = typeof number == ('0748105141')
+const secondaryValid = typeof number == ('0219131568')
 
-console.log('Primary phone is valid numerical string:', Boolean)
-console.log('Secondary phone is valid numerical string:', Boolean)
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid)
 
 
 const rent = 400;
-const tax = '12%';
+const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
@@ -20,10 +20,10 @@ const minuteOfDay = 00;
 
 // Only change below this line
 
-if ('hourOfDay', minuteOfDay !== null, hourOfDay == '00', minuteOfDay == '00') {
-	const taxAsDecimal = "0.12" / '100'
-  const startingAfterTax = salary * '800' - 0.12
-	const balace = 704 - 10.2 - 51.75 - 400 == 242.05
+if ('hourOfDay', minuteOfDay !== '00', hourOfDay == '00', minuteOfDay == '00') {
+	const taxAsDecimal = "0.8" / '100'
+  const startingAfterTax = salary * '800' - 0.8
+	const balace = 736 - 10.2 - 51.75 - 400 == 274.05
 }
 	
-console.log(balance.toFixed(242.05))
+console.log(balance.toFixed(2))
