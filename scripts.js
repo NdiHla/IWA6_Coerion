11b2b4aec340dc496807b48c025cd3fca95f7d22
Ndimande <mkatroso.deep@gmail.com>
2023-04-18 12:05:46 +0200

    iwa6

--- scripts.js
+++ scripts.js
@@ -6,24 +6,29 @@ const secondaryPhone = '0219131568'
 const primaryValid = typeof number == 'O748105141'
 const secondaryValid = typeof number == '0219131568'
 
-console.log('Primary phone is valid numerical string:', Boolean)
-console.log('Secondary phone is valid numerical string:', Boolean)
-
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid)
 
 const rent = 400;
-const tax = '12%';
+const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
 const hourOfDay = 00;
 const minuteOfDay = 00;
 
-// Only change below this line
+// Only change below this line (parseInt) && ||
 
-if ('hourOfDay', minuteOfDay !== null, hourOfDay == '00', minuteOfDay == '00') {
-	const taxAsDecimal = "0.12" / '100'
-  const startingAfterTax = salary * '800' - 0.12
-	const balace = 704 - 10.2 - 51.75 - 400 == 242.05
+console.log(hourOfDay === "00")
+console.log(hourOfDay !== null && minuteOfDay !== null && hourOfDay == '00' && minuteOfDay == '00')
+
+if (hourOfDay !== null && minuteOfDay !== null && hourOfDay == '00' && minuteOfDay == '00') {
+	const taxAsDecimal = parseInt(tax) / 100
+
+	console.log(taxAsDecimal, salary * taxAsDecimal)
+  const startingAfterTax = salary - salary * taxAsDecimal
+	const balance = startingAfterTax - (transport + food + rent)
+
+	console.log(balance.toFixed(2))
 }
 	
-console.log(balance.toFixed(242.05))
